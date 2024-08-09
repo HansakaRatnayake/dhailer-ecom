41e4aea482634177d918987a918f326e2f951217
hansakarathnayake <hndk2002@gmail.com>
2024-08-10 01:48:56 +0530

    exception handling

---
+++ src/main/java/com/eCom/dhailer/adviser/AppWideExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.eCom.dhailer.adviser;
+
+import com.eCom.dhailer.exception.EntryNotFoundException;
+import com.eCom.dhailer.util.StandardResponce;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class AppWideExceptionHandler {
+
+    @ExceptionHandler(EntryNotFoundException.class)
+    public ResponseEntity<StandardResponce> handlerEntryNotFoundException(EntryNotFoundException e){
+        return new ResponseEntity<>(
+                new StandardResponce(404, e.getMessage(), e),
+                HttpStatus.NOT_FOUND
+        );
+    }
+}
